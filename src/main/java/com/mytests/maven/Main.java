c963bab7329013f83b66ff8dc5160d7e69ee03b9
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-09-08 11:14:30 +0200

    Initial commit

---
+++ src/main/java/com/mytests/maven/Main.java
@@ -0,0 +1,11 @@
+package com.mytests.maven;/**
+ **
+ * <p>Created by ${USER} on ${DATE}.</p>
+ * <p>Project: ${PROJECT_NAME}</p>
+ **
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
