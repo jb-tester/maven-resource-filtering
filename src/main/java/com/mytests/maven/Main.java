4ad97c9118e91dc5efaebb7d51599a0a87dffacd
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-09-08 11:16:43 +0200

    complete

--- src/main/java/com/mytests/maven/Main.java
+++ src/main/java/com/mytests/maven/Main.java
@@ -1,11 +1,27 @@
-package com.mytests.maven;/**
- **
+package com.mytests.maven;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * *
  * <p>Created by ${USER} on ${DATE}.</p>
  * <p>Project: ${PROJECT_NAME}</p>
- **
+ * *
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale locale = new Locale("en", "En");
+        ResourceBundle exampleBundle = ResourceBundle.getBundle("messages", locale);
+
+        System.out.println(exampleBundle.getString("p1"));
+        System.out.println(exampleBundle.getString("p2"));
+        System.out.println(exampleBundle.getString("p3"));
+        System.out.println(exampleBundle.getString("p4"));
+        System.out.println(exampleBundle.getString("p5"));
+        System.out.println(exampleBundle.getString("p6"));
+        System.out.println(exampleBundle.getString("p7"));
+        System.out.println(exampleBundle.getString("p8"));
+        System.out.println(exampleBundle.getString("p9"));
     }
 }
